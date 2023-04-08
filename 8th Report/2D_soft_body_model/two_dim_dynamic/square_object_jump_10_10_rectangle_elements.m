df3f30e13d589ff10c8ce8b306ec8da2556ef919
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-08 23:18:37 +0900

    update

--- 8th Report/2D_soft_body_model/two_dim_dynamic/square_object_jump_10_10_rectangle_elements.m
+++ 8th Report/2D_soft_body_model/two_dim_dynamic/square_object_jump_10_10_rectangle_elements.m
@@ -1,3 +1,9 @@
 % Jumping of an elastic square object (10&times;10) rectangular elements
 % �����`�e�����̂̒��� (10&times;10) �����`�v�f
 % g, cm, sec
+
+addpath('../two_dim_fea');
+
+idth = 30; height = 30; thickness = 1;
+m = 10; n = 10;
+[ points, triangles, rectangles ] = rectangular_object(m, n, width, height);
