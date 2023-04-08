f9ad14530dbe696b6ea87d590a35eaf6f0371e9a
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-08 23:19:57 +0900

    update

--- 8th Report/2D_soft_body_model/two_dim_dynamic/square_object_jump_10_10_rectangle_elements.m
+++ 8th Report/2D_soft_body_model/two_dim_dynamic/square_object_jump_10_10_rectangle_elements.m
@@ -8,4 +8,10 @@ width = 30; height = 30; thickness = 1;
 m = 10; n = 10;
 [ points, triangles, rectangles ] = rectangular_object(m, n, width, height);
 
+% E = 1 MPa; c = 0.04 kPa s; rho = 1 g/cm^3
+Young = 10.0*1e+6; c = 0.4*1e+3; nu = 0.48; density = 1.00;
+[lambda, mu] = Lame_constants(Young, nu);
+[lambda_vis, mu_vis] = Lame_constants(c, nu);
+
+
 
