beaf0c71e4859e1ada078bed5dd514cc6e3d1e29
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-10 14:05:42 +0900

    update

--- 8th Report/2D_soft_body_model/Body.m
+++ 8th Report/2D_soft_body_model/Body.m
@@ -278,3 +278,25 @@ classdef Body
                 obj.Damping_Matrix(loc,loc) = obj.Damping_Matrix(loc,loc) + B_p;
             end
         end
+
+        function obj = calculate_inertia_matrix(obj)
+            obj.Inertia_Matrix = zeros(2*obj.numNodalPoints, 2*obj.numNodalPoints);
+            %
+            for p=1:obj.numTriangles
+                tri = obj.Triangles(p);
+                vs = tri.Vertices;
+                i = vs(1); j = vs(2); k = vs(3);
+                [obj.Triangles(p), M_p] = tri.partial_inertia_matrix;
+                loc = [ 2*i-1, 2*i, 2*j-1, 2*j, 2*k-1, 2*k ];
+                obj.Inertia_Matrix(loc,loc) = obj.Inertia_Matrix(loc,loc) + M_p;
+            end
+            %
+            for p=1:obj.numRectangles
+                rct = obj.Rectangles(p);
+                vs = rct.Vertices;
+                i = vs(1); j = vs(2); k = vs(3); l = vs(4);
+                [obj.Rectangles(p), M_p] = rct.partial_inertia_matrix;
+                loc = [ 2*i-1, 2*i, 2*j-1, 2*j, 2*k-1, 2*k, 2*l-1, 2*l ];
+                obj.Inertia_Matrix(loc,loc) = obj.Inertia_Matrix(loc,loc) + M_p;
+            end
+        end
