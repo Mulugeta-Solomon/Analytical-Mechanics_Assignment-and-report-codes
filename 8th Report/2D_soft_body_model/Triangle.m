647a80b975547539e23e37ff7a7c7dc7a1a8fc0d
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-11 13:47:27 +0900

    update]

--- 8th Report/2D_soft_body_model/Triangle.m
+++ 8th Report/2D_soft_body_model/Triangle.m
@@ -117,3 +117,10 @@ classdef Triangle
                 [2*I, I, I; I, 2*I, I; I, I, 2*I];
         end
 
+        function [obj, M_p] = partial_inertia_matrix(obj)
+            if isempty( obj.Partial_Inertia_Matrix )
+                obj = obj.calculate_partial_inertia_matrix;
+            end
+            M_p = obj.Partial_Inertia_Matrix;
+        end
+
