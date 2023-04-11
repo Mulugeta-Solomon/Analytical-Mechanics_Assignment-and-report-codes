3f59b280a3f7e1c4ebfa6c775a942faa38b39e6f
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-11 13:37:35 +0900

    update]

--- 8th Report/2D_soft_body_model/Rectangle.m
+++ 8th Report/2D_soft_body_model/Rectangle.m
@@ -11,3 +11,35 @@ classdef Rectangle
         Partial_Damping_Matrix;
         Partial_Inertia_Matrix;
     end
+    methods
+        function obj = Rectangle (i, j, k, l, lx, ly, h)
+            obj.Vertices = [ i, j, k, l ];
+            obj.length_x = lx; obj.length_y = ly;
+            obj.Area = lx*ly;
+            obj.Thickness = h;
+            
+            luu = (h/6)*(ly/lx)* ...
+                [ 2, -2, -1,  1;
+                 -2,  2,  1, -1;
+                 -1,  1,  2, -2;
+                  1, -1, -2,  2 ];
+            luv = (h/4)* ...
+                [ 1,  1, -1, -1;
+                 -1, -1,  1,  1;
+                 -1, -1,  1,  1;
+                  1,  1, -1, -1 ];
+            lvu = luv';
+            lvv = (h/6)*(lx/ly)* ...
+                [ 2,  1, -1, -2;
+                  1,  2, -2, -1;
+                 -1, -2,  2,  1;
+                 -2, -1,  1,  2 ];
+            muu = 2*luu + lvv; muv = lvu;
+            mvu = luv; mvv = 2*lvv + luu;
+            
+            l = [ luu, luv; lvu, lvv ];
+            m = [ muu, muv; mvu, mvv ];
+            
+            obj.Partial_J_lambda = l([1,5,2,6,3,7,4,8], [1,5,2,6,3,7,4,8]);
+            obj.Partial_J_mu     = m([1,5,2,6,3,7,4,8], [1,5,2,6,3,7,4,8]);
+        end
