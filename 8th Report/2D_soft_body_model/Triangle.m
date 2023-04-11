a112a18d798a76c611e198c3fc30c414baf0b79e
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-11 13:44:14 +0900

    update]

--- 8th Report/2D_soft_body_model/Triangle.m
+++ 8th Report/2D_soft_body_model/Triangle.m
@@ -15,4 +15,25 @@ classdef Triangle
         Partial_Inertia_Matrix;
         Partial_Gravitational_Vector;
     end
+    methods
+        function obj = Triangle(i, j, k, pi, pj, pk, h)
+            obj.Vertices = [ i, j, k ];
+            obj.Area = det( [ pj-pi, pk-pi ] )/2;
+            obj.Thickness = h;
+            obj.vector_a = ( 1/(2*obj.Area))*[ pj(2)-pk(2); pk(2)-pi(2); pi(2)-pj(2) ];
+            obj.vector_b = (-1/(2*obj.Area))*[ pj(1)-pk(1); pk(1)-pi(1); pi(1)-pj(1) ];
+            
+            a = obj.vector_a;
+            b = obj.vector_b;
+            vol = obj.Area * obj.Thickness;
+            luu = vol*a*a'; luv = vol*a*b';
+            lvu = vol*b*a'; lvv = vol*b*b';
+            muu = 2*luu + lvv; muv = lvu;
+            mvu = luv; mvv = 2*lvv + luu;
+            
+            l = [ luu, luv; lvu, lvv ];
+            m = [ muu, muv; mvu, mvv ];
     
+            obj.Partial_J_lambda = l([1,4,2,5,3,6], [1,4,2,5,3,6]);
+            obj.Partial_J_mu     = m([1,4,2,5,3,6], [1,4,2,5,3,6]);
+        end
