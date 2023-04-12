486a3b899891fe2772a34ef18634456114ca6e5d
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-12 12:09:51 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/Body_ThreeElementModel.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/Body_ThreeElementModel.m
@@ -2,3 +2,13 @@ classdef Body_ThreeElementModel < Body
     properties
         lambda_vis_1, mu_vis_1, lambda_vis_2, mu_vis_2;
     end
+
+    methods
+        function obj = Body_ThreeElementModel(npoints, points, ntris, tris, h)
+            obj@Body(npoints, points, ntris, tris, h);
+            for p=1:ntris
+                i = tris(p,1); j = tris(p,2); k = tris(p,3);
+                tr(p) = Triangle_ThreeElementModel(i, j, k, points(:,i), points(:,j), points(:,k), h);
+            end
+            obj.Triangles = tr;
+        end
