577e6547630e22835e94e8cfa83eaced7990326a
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-13 16:10:01 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_4_4.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_4_4.m
@@ -10,3 +10,9 @@ m = 4; n = 4;
 npoints = size(points,2);
 ntriangles = size(triangles,1);
 elastoplastic = Body_ThreeElementModel(npoints, points, ntriangles, triangles, thickness);
+
+% E = 1 MPa; c1 = 0.04 kPa s; c2 = 2 MPa s; rho = 1 g/cm^3
+Young = 10.0*1e+6; c1 = 0.4*1e+3; c2 = 20*1e+6; nu = 0.48; density = 1.00;
+[lambda, mu] = Lame_constants(Young, nu);
+[lambda_vis_1, mu_vis_1] = Lame_constants(c1, nu);
+[lambda_vis_2, mu_vis_2] = Lame_constants(c2, nu);
