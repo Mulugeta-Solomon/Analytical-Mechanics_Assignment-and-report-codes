8055c18ab66dbaecace05c6cb82061750059d4cb
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-13 16:09:35 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_4_4.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_4_4.m
@@ -4,3 +4,9 @@
 
 addpath('../two_dim_fea');
 
+width = 30; height = 30; thickness = 1;
+m = 4; n = 4;
+[points, triangles] = rectangular_object(m, n, width, height);
+npoints = size(points,2);
+ntriangles = size(triangles,1);
+elastoplastic = Body_ThreeElementModel(npoints, points, ntriangles, triangles, thickness);
