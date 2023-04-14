6971d2d36ae509ca4b439f2261f1be1537b3a2b8
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:15:59 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
@@ -113,3 +113,30 @@ v = VideoWriter('square_object_three_element_model_10_10', 'MPEG-4');
 open(v);
 writeVideo(v, M);
 close(v);
+
+function dotq = square_object_constraint_param(t,q, body, A,b0,b1, alpha)
+    disp(t);
+    
+    persistent npoints lambda mu lambdav1 muv1 lambdav2 muv2 M Jlambda Jmu;
+    if isempty(npoints)
+        npoints = body.numNodalPoints;
+        lambda = body.lambda; mu = body.mu;
+        lambdav1 = body.lambda_vis_1; muv1 = body.mu_vis_1;
+        lambdav2 = body.lambda_vis_2; muv2 = body.mu_vis_2;
+        M = body.Inertia_Matrix;
+        Jlambda = body.J_lambda;
+        Jmu     = body.J_mu;
+    end
+    
+    un = q(1:2*npoints);
+    vn = q(2*npoints+1:4*npoints);
+    flambda = q(4*npoints+1:6*npoints);
+    fmu     = q(6*npoints+1:8*npoints);
+    
+    dotun = vn;
+    
+    coef = [ M, -A; -A', zeros(size(A,2),size(A,2))];
+    vec = [ -flambda-fmu; 2*alpha*(A'*vn-b1)+(alpha^2)*(A'*un-(b0+b1*t)) ];
+    sol = coef\vec;
+    dotvn = sol(1:2*npoints);
+    
