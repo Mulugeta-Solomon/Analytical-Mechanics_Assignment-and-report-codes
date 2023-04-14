4c7a51b7665f8d96728006d4dca2d20be2d949c3
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:12:39 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
@@ -1,3 +1,12 @@
 % Dynamic deformation of an elasto-plastic square object (10&times;10)
 % �����`�e�Y�����̂̓��I�ȕό` (10&times;10)
 % g, cm, sec
+
+addpath('../two_dim_fea');
+
+width = 30; height = 30; thickness = 1;
+m = 10; n = 10;
+[points, triangles] = rectangular_object(m, n, width, height);
+npoints = size(points,2);
+ntriangles = size(triangles,1);
+elastoplastic = Body_ThreeElementModel(npoints, points, ntriangles, triangles, thickness);
