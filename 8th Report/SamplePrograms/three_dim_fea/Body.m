857935b0345384c4f48c93dfbbc8d9e501d8387a
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:22:27 +0900

    update

--- 8th Report/SamplePrograms/three_dim_fea/Body.m
+++ 8th Report/SamplePrograms/three_dim_fea/Body.m
@@ -14,3 +14,20 @@ classdef Body
         Inertia_Matrix;
         Gravitational_Vector;
     end
+
+    methods
+        function obj = Body(npoints, points, ntetras, tetras)
+            obj.numNodalPoints = npoints;
+            for k=1:npoints
+                pt(k) = NodalPoint(points(:,k));
+            end
+            obj.NodalPoints = pt;
+            %
+            if ntetras > 0
+                obj.numTetrahedra = ntetras;
+                for p=1:ntetras
+                    i = tetras(p,1); j = tetras(p,2); k = tetras(p,3); l = tetras(p,4);
+                    te(p) = Tetrahedron(i, j, k, l, points(:,i), points(:,j), points(:,k), points(:,l));
+                end
+                obj.Tetrahedra = te;
+            end
