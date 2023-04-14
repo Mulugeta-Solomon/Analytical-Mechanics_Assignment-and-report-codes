bcc753c56939bde36495bb6fb121b999152d00ac
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:13:28 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
@@ -21,3 +21,7 @@ Young = 10.0*1e+6; c1 = 0.4*1e+3; c2 = 20*1e+6; nu = 0.48; density = 1.00;
 elastoplastic = elastoplastic.mechanical_parameters(density, lambda, mu, lambda_vis_1, mu_vis_1, lambda_vis_2, mu_vis_2);
 elastoplastic = elastoplastic.calculate_stiffness_matrix;
 elastoplastic = elastoplastic.calculate_inertia_matrix;
+
+tp = 1.0; vpush = 0.8*(height/3)/tp;
+th = 1.0;
+tf = 5.0;
