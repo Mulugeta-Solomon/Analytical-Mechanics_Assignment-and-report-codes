8f39e085dd15dee31c926c9aa5aa500091f7d74b
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:13:14 +0900

    update

--- 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
+++ 8th Report/SamplePrograms/inelastic_two_dim/square_object_three_element_model_10_10.m
@@ -17,3 +17,7 @@ Young = 10.0*1e+6; c1 = 0.4*1e+3; c2 = 20*1e+6; nu = 0.48; density = 1.00;
 [lambda, mu] = Lame_constants(Young, nu);
 [lambda_vis_1, mu_vis_1] = Lame_constants(c1, nu);
 [lambda_vis_2, mu_vis_2] = Lame_constants(c2, nu);
+
+elastoplastic = elastoplastic.mechanical_parameters(density, lambda, mu, lambda_vis_1, mu_vis_1, lambda_vis_2, mu_vis_2);
+elastoplastic = elastoplastic.calculate_stiffness_matrix;
+elastoplastic = elastoplastic.calculate_inertia_matrix;
