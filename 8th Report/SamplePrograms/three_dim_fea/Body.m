5ff40371c89a481474d4b6394f17de582eacec76
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:24:59 +0900

    update

--- 8th Report/SamplePrograms/three_dim_fea/Body.m
+++ 8th Report/SamplePrograms/three_dim_fea/Body.m
@@ -142,3 +142,17 @@ classdef Body
             end
             %
         end
+
+        function obj = calculate_damping_matrix(obj)
+            obj.Damping_Matrix = zeros(3*obj.numNodalPoints, 3*obj.numNodalPoints);
+            %
+            for p=1:obj.numTetrahedra
+                tetra = obj.Tetrahedra(p);
+                vs = tetra.Vertices;
+                i = vs(1); j = vs(2); k = vs(3); l = vs(4);
+                [obj.Tetrahedra(p), B_p] = tetra.partial_damping_matrix;
+                loc = [ 3*i-2, 3*i-1, 3*i, 3*j-2, 3*j-1, 3*j, 3*k-2, 3*k-1, 3*k, 3*l-2, 3*l-1, 3*l ];
+                obj.Damping_Matrix(loc,loc) = obj.Damping_Matrix(loc,loc) + B_p;
+            end
+            %
+        end
