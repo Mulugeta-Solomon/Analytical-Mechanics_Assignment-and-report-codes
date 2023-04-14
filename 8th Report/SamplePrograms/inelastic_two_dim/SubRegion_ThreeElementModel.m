89acd12dcba3a62cb230bf09bb4c2c1b713253dc
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:16:47 +0900

    update

---
+++ 8th Report/SamplePrograms/inelastic_two_dim/SubRegion_ThreeElementModel.m
@@ -0,0 +1,8 @@
+classdef SubRegion_ThreeElementModel < SubRegion
+    properties
+        lambda_vis_1; mu_vis_1; lambda_vis_2; mu_vis_2;
+        suffixes_for_nodal_points;
+    end
+    methods
+    end
+end
