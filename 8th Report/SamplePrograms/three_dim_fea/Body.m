7bc1c4447f760dc1dc3e66f7a228481611b011b7
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-14 12:22:41 +0900

    update

--- 8th Report/SamplePrograms/three_dim_fea/Body.m
+++ 8th Report/SamplePrograms/three_dim_fea/Body.m
@@ -31,3 +31,13 @@ classdef Body
                 end
                 obj.Tetrahedra = te;
             end
+            obj.J_lambda = zeros(3*obj.numNodalPoints, 3*obj.numNodalPoints);
+            obj.J_mu     = zeros(3*obj.numNodalPoints, 3*obj.numNodalPoints);
+            for p=1:obj.numTetrahedra
+                tetra = obj.Tetrahedra(p);
+                vs = tetra.Vertices;
+                i = vs(1); j = vs(2); k = vs(3); l = vs(4);
+                loc = [ 3*i-2, 3*i-1, 3*i, 3*j-2, 3*j-1, 3*j, 3*k-2, 3*k-1, 3*k, 3*l-2, 3*l-1, 3*l ];
+                obj.J_lambda(loc,loc) = obj.J_lambda(loc,loc) + tetra.Partial_J_lambda;
+                obj.J_mu(loc,loc)     = obj.J_mu(loc,loc)     + tetra.Partial_J_mu;
+            end
