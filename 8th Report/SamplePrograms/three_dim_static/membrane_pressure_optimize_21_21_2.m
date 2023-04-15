0d42ee1712222cfa5356824e0cf7ce92f8c51786
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-16 02:40:33 +0900

    update

--- 8th Report/SamplePrograms/three_dim_static/membrane_pressure_optimize_21_21_2.m
+++ 8th Report/SamplePrograms/three_dim_static/membrane_pressure_optimize_21_21_2.m
@@ -9,3 +9,8 @@ addpath('../three_dim_fea');
 % E = 0.1 MPa; c = 0.04 kPa s; rho = 1 g/cm^2
 Young = 1.0*1e+6; c = 0.4*1e+3; nu = 0.48; density = 1.00;
 [ lambda, mu ] = Lame_constants( Young, nu );
+
+l = 16; m = 16; n = 2; membrane_size = 10; thickness = 0.5;
+% p = 2e+4 = 0.02e+6 --> 0.002 MPa = 2 KPa
+pressure = 40e+4;
+%pressure = 80e+4;
