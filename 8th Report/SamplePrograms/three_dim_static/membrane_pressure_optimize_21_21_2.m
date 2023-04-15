bbc6a486b7c99d63f7988e7bc4815a0fc2ebf35c
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-16 02:40:18 +0900

    update

--- 8th Report/SamplePrograms/three_dim_static/membrane_pressure_optimize_21_21_2.m
+++ 8th Report/SamplePrograms/three_dim_static/membrane_pressure_optimize_21_21_2.m
@@ -5,3 +5,7 @@
 
 dt0 = datetime;
 addpath('../three_dim_fea');
+
+% E = 0.1 MPa; c = 0.04 kPa s; rho = 1 g/cm^2
+Young = 1.0*1e+6; c = 0.4*1e+3; nu = 0.48; density = 1.00;
+[ lambda, mu ] = Lame_constants( Young, nu );
