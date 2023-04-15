17e1d2e350a95a32ea3172e04660cd02a168c411
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-15 11:17:14 +0900

    update

--- 8th Report/SamplePrograms/three_dim_static/beam_twisting_Cauchy_strain.m
+++ 8th Report/SamplePrograms/three_dim_static/beam_twisting_Cauchy_strain.m
@@ -9,3 +9,10 @@ l = 2; m = 2; n = 5;
 cube_size = 1;
 rotation_angle = 20*2*pi/360;   % 20 degrees
 
+
+[points, tetrahedra ] = cuboidal_object( l, m, n, cube_size*(l-1), cube_size*(m-1), cube_size*(n-1) );
+
+% E = 0.1 MPa; rho = 0.020 g/cm^2
+Young = 1.0*1e+6; nu = 0.48; density = 0.020;
+% 10.0 N --> fpush = 10.0*1e+5;
+%force = [0; -1.2*1e+5];
