d0c7f3e649f3f04841e99b86662e9bae13a974b4
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-17 11:31:42 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/beam_bending_push_Green_strain.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/beam_bending_push_Green_strain.m
@@ -11,3 +11,12 @@ Young = 1.0*1e+6; c = 0.04*1e+3; nu = 0.48; density = 0.020;
 
 [ lambda, mu ] = Lame_constants( Young, nu );
 [lambda_vis, mu_vis] = Lame_constants(c, nu);
+
+npoints = size(points,2);
+ntriangles = size(triangles,1);
+elastic = Body(npoints, points, ntriangles, triangles, thickness);
+elastic = elastic.mechanical_parameters(density, lambda, mu);
+elastic = elastic.viscous_parameters(lambda_vis, mu_vis);
+elastic = elastic.calculate_stiffness_matrix;
+elastic = elastic.calculate_damping_matrix;
+elastic = elastic.calculate_inertia_matrix;
