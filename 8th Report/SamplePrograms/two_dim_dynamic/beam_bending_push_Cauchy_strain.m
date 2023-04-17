8539d96a5fccacece23bb15dd90ff7e03fc47c51
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-17 11:16:30 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/beam_bending_push_Cauchy_strain.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/beam_bending_push_Cauchy_strain.m
@@ -5,3 +5,22 @@ addpath('../two_dim_fea');
 width = 10; height = 2; thickness = 1;
 m = 11; n = 3;
 [ points, triangles ] = rectangular_object( m, n, width, height );
+
+ E = 0.1 MPa; c = 0.004 kPa s; rho = 0.020 g/cm^3
+Young = 1.0*1e+6; c = 0.04*1e+3; nu = 0.48; density = 0.020;
+
+[ lambda, mu ] = Lame_constants( Young, nu );
+[lambda_vis, mu_vis] = Lame_constants(c, nu);
+
+npoints = size(points,2);
+ntriangles = size(triangles,1);
+elastic = Body(npoints, points, ntriangles, triangles, thickness);
+elastic = elastic.mechanical_parameters(density, lambda, mu);
+elastic = elastic.viscous_parameters(lambda_vis, mu_vis);
+elastic = elastic.calculate_stiffness_matrix;
+elastic = elastic.calculate_damping_matrix;
+elastic = elastic.calculate_inertia_matrix;
+
+tp = 0.3; upush = 0; vpush = -2.5*height/tp;
+th = 0.2;
+tf = 1.5;
