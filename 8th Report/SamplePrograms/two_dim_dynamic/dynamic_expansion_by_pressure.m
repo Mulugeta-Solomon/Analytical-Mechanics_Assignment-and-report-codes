4c387a73e8c220e120aeaacab521d3668100ab73
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-18 18:28:51 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/dynamic_expansion_by_pressure.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/dynamic_expansion_by_pressure.m
@@ -13,3 +13,12 @@ Young = 1.0*1e+6; c = 0.4*1e+3; nu = 0.48; density = 1.00;
 mrect = 11; nrect = 2;
 [ points, triangles ] = rectangular_object( mrect, nrect, 10, 1 );
 thickness = 1;
+
+npoints = size(points,2);
+ntriangles = size(triangles,1);
+elastic = Body(npoints, points, ntriangles, triangles, thickness);
+elastic = elastic.mechanical_parameters(density, lambda, mu);
+elastic = elastic.viscous_parameters(lambda_vis, mu_vis);
+elastic = elastic.calculate_stiffness_matrix;
+elastic = elastic.calculate_damping_matrix;
+elastic = elastic.calculate_inertia_matrix;
