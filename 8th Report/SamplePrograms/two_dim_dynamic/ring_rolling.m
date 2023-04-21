ed5ffa63d7f784fffeac219b29c54d51660ddead
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-21 09:55:14 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/ring_rolling.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/ring_rolling.m
@@ -6,3 +6,7 @@ addpath('../two_dim_fea');
 m = 16; n = 3; router = 4; rinner = 2; thickness = 1;
 [points, triangles] = ring_object(m, n, router, rinner);
 points = points + [ 0; router ];
+
+npoints = size(points, 2);
+ntriangles = size(triangles, 1);
+ring = Body(npoints, points, ntriangles, triangles, thickness);
