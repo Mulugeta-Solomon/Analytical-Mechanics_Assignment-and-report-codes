fff7d458ca3d6ddc27272eb0074409bf3d3a6900
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-21 09:54:56 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/ring_rolling.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/ring_rolling.m
@@ -2,3 +2,7 @@
 
 dt0 = datetime;
 addpath('../two_dim_fea');
+
+m = 16; n = 3; router = 4; rinner = 2; thickness = 1;
+[points, triangles] = ring_object(m, n, router, rinner);
+points = points + [ 0; router ];
