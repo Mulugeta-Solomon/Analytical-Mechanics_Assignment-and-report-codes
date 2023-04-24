1a5a8cbf54bef341ca9814dd41ac6b0b7cbda64d
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-24 10:43:27 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/square_object_4_4.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/square_object_4_4.m
@@ -1,3 +1,9 @@
 % Dynamic deformation of an elastic square object (4&times;4)
 % �����`�e�����̂̓��I�ȕό` (4&times;4)
 % g, cm, sec
+
+addpath('../two_dim_fea');
+
+width = 30; height = 30; thickness = 1;
+m = 4; n = 4;
+[points, triangles] = rectangular_object(m, n, width, height);
