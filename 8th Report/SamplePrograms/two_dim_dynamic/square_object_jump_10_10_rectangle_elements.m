e22a2a01e6bc126ae947d5313854f309111b48e1
Mulugeta-Solomon <msolomon759@gmail.com>
2023-04-30 22:52:03 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/square_object_jump_10_10_rectangle_elements.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/square_object_jump_10_10_rectangle_elements.m
@@ -108,3 +108,8 @@ for t = 0:0.01:tp+th+tf
     disp(t);
 end
 M(fr) = getframe(gcf);
+
+v = VideoWriter('square_object_jump_10_10_rectangle_elements', 'MPEG-4');
+open(v);
+writeVideo(v, M);
+close(v);
