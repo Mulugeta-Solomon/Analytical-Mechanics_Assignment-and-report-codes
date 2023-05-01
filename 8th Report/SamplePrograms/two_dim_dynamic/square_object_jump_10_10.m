de429a2358e93f4628ce89539c27c40cdadfaa27
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-01 22:39:07 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/square_object_jump_10_10.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/square_object_jump_10_10.m
@@ -13,3 +13,13 @@ m = 10; n = 10;
 Young = 10.0*1e+6; c = 0.4*1e+3; nu = 0.48; density = 1.00;
 [lambda, mu] = Lame_constants(Young, nu);
 [lambda_vis, mu_vis] = Lame_constants(c, nu);
+
+
+npoints = size(points,2);
+ntriangles = size(triangles,1);
+elastic = Body(npoints, points, ntriangles, triangles, thickness);
+elastic = elastic.mechanical_parameters(density, lambda, mu);
+elastic = elastic.viscous_parameters(lambda_vis, mu_vis);
+elastic = elastic.calculate_stiffness_matrix;
+elastic = elastic.calculate_damping_matrix;
+elastic = elastic.calculate_inertia_matrix;
