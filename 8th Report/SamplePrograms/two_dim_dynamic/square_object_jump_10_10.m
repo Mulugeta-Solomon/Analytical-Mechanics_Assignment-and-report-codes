a76bb6ac2410e0524da2bc000d44f5b8c8e7354b
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-02 14:27:17 +0900

    update

--- 8th Report/SamplePrograms/two_dim_dynamic/square_object_jump_10_10.m
+++ 8th Report/SamplePrograms/two_dim_dynamic/square_object_jump_10_10.m
@@ -160,5 +160,15 @@ function dotq = square_object_free_param(t,q, body)
 
     un = q(1:2*npoints);
     vn = q(2*npoints+1:4*npoints);
+
+    dotun = vn;
+    
+    coef = M;
+    vec = -K*un-B*vn + contact_force(t, body, un, vn);
+    sol = coef\vec;
+    dotvn = sol(1:2*npoints);
+    
+    dotq = [dotun; dotvn];
+end
     
     
