7dc5d7ef1ecfda13740a9749beb7ba00c6b24e29
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-03 21:16:43 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/Body.m
+++ 8th Report/SamplePrograms/two_dim_fea/Body.m
@@ -5,3 +5,15 @@ classdef Body
         numRectangles; Rectangles;
         Thickness;
         BoundaryEdges;
+        numSubRegions; SubRegions;
+        numContours; Contours;
+        Density; lambda; mu; lambda_vis; mu_vis;
+        strain_potential_energy;
+        gravitational_potential_energy;
+        J_lambda; J_mu;
+        Stiffness_Matrix;
+        Damping_Matrix;
+        Inertia_Matrix;
+        Gravitational_Vector;
+        coef_order_one; coef_order_two; c2i; c2j; coef_order_three; c3i; c3j; c3k; % coefficient matrices for Green strain based forces
+    end
