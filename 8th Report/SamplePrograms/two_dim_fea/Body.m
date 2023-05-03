ada513754446d067d6f76409e5b78ba7692de5c1
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-03 21:17:46 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/Body.m
+++ 8th Report/SamplePrograms/two_dim_fea/Body.m
@@ -17,20 +17,20 @@ classdef Body
         Gravitational_Vector;
         coef_order_one; coef_order_two; c2i; c2j; coef_order_three; c3i; c3j; c3k; % coefficient matrices for Green strain based forces
     end
-methods
-        function obj = Body(npoints, points, ntris, tris, h)
-            obj.numNodalPoints = npoints;
-            for k=1:npoints
-                pt(k) = NodalPoint(points(:,k));
-            end
-            obj.NodalPoints = pt;
-            %
-            if ntris > 0
-                obj.numTriangles = ntris;
-                for p=1:ntris
-                    i = tris(p,1); j = tris(p,2); k = tris(p,3);
-                    tr(p) = Triangle(i, j, k, points(:,i), points(:,j), points(:,k), h);
+    methods
+            function obj = Body(npoints, points, ntris, tris, h)
+                obj.numNodalPoints = npoints;
+                for k=1:npoints
+                    pt(k) = NodalPoint(points(:,k));
                 end
-                obj.Triangles = tr;
-            end
-            %
+                obj.NodalPoints = pt;
+                %
+                if ntris > 0
+                    obj.numTriangles = ntris;
+                    for p=1:ntris
+                        i = tris(p,1); j = tris(p,2); k = tris(p,3);
+                        tr(p) = Triangle(i, j, k, points(:,i), points(:,j), points(:,k), h);
+                    end
+                    obj.Triangles = tr;
+                end
+                %
