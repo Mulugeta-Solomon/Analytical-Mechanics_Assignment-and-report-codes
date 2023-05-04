ba01a0644d7224e0fa5e8bdb067be221f5cc5951
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-05 01:50:36 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/Body.m
+++ 8th Report/SamplePrograms/two_dim_fea/Body.m
@@ -186,5 +186,24 @@ classdef Body
                 disps = zeros(2, obj.numNodalPoints);
                 color = [0.9, 0.9, 0.9];
             end
+            for p=1:obj.numTriangles
+                tri = obj.Triangles(p);
+                vs = tri.Vertices;
+                i = vs(1); j = vs(2); k = vs(3);
+                np = obj.NodalPoints;
+                xi = np(i).Coordinates + disps(:,i);
+                xj = np(j).Coordinates + disps(:,j);
+                xk = np(k).Coordinates + disps(:,k);
+                points = [ xi, xj, xk, xi ];
+                plot(points(1,:), points(2,:), 'k-');
+                if isempty(obj.Triangles(p).color)
+                    fill(points(1,:), points(2,:), color);
+                else
+                    fill(points(1,:), points(2,:), obj.Triangles(p).color)
+                end
+                hold on;
+            end
+        end
+        
         
         
