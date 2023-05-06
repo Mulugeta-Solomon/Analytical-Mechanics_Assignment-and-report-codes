9ad8fbb8cdfdd91a68cd10a14dc10a04de807781
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-07 00:48:52 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/Body.m
+++ 8th Report/SamplePrograms/two_dim_fea/Body.m
@@ -265,5 +265,14 @@ classdef Body
                 obj.Damping_Matrix(loc,loc) = obj.Damping_Matrix(loc,loc) + B_p;
             end
             %
+            for p=1:obj.numRectangles
+                rct = obj.Rectangles(p);
+                vs = rct.Vertices;
+                i = vs(1); j = vs(2); k = vs(3); l = vs(4);
+                [obj.Rectangles(p), B_p] = rct.partial_damping_matrix;
+                loc = [ 2*i-1, 2*i, 2*j-1, 2*j, 2*k-1, 2*k, 2*l-1, 2*l ];
+                obj.Damping_Matrix(loc,loc) = obj.Damping_Matrix(loc,loc) + B_p;
+            end
+        end
         
         
