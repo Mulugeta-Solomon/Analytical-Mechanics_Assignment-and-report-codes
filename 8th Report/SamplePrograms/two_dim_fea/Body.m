33a95295f03f3173090b08a2dc0c6bcbcba71fe6
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-07 00:50:01 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/Body.m
+++ 8th Report/SamplePrograms/two_dim_fea/Body.m
@@ -287,4 +287,14 @@ classdef Body
                 obj.Inertia_Matrix(loc,loc) = obj.Inertia_Matrix(loc,loc) + M_p;
             end
             %
+            for p=1:obj.numRectangles
+                rct = obj.Rectangles(p);
+                vs = rct.Vertices;
+                i = vs(1); j = vs(2); k = vs(3); l = vs(4);
+                [obj.Rectangles(p), M_p] = rct.partial_inertia_matrix;
+                loc = [ 2*i-1, 2*i, 2*j-1, 2*j, 2*k-1, 2*k, 2*l-1, 2*l ];
+                obj.Inertia_Matrix(loc,loc) = obj.Inertia_Matrix(loc,loc) + M_p;
+            end
+        end
+        
         
