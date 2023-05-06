668d9427fb037bb9e4f3fca94e571f8111e03d44
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-07 00:48:32 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/Body.m
+++ 8th Report/SamplePrograms/two_dim_fea/Body.m
@@ -256,5 +256,14 @@ classdef Body
         function obj = calculate_damping_matrix(obj)
             obj.Damping_Matrix = zeros(2*obj.numNodalPoints, 2*obj.numNodalPoints);
             %
+            for p=1:obj.numTriangles
+                tri = obj.Triangles(p);
+                vs = tri.Vertices;
+                i = vs(1); j = vs(2); k = vs(3);
+                [obj.Triangles(p), B_p] = tri.partial_damping_matrix;
+                loc = [ 2*i-1, 2*i, 2*j-1, 2*j, 2*k-1, 2*k ];
+                obj.Damping_Matrix(loc,loc) = obj.Damping_Matrix(loc,loc) + B_p;
+            end
+            %
         
         
