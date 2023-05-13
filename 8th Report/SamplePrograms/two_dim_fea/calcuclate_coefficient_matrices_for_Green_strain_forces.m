f589344bd225617e05fadcf5b8995763850657c9
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-13 22:03:25 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/calcuclate_coefficient_matrices_for_Green_strain_forces.m
+++ 8th Report/SamplePrograms/two_dim_fea/calcuclate_coefficient_matrices_for_Green_strain_forces.m
@@ -17,3 +17,25 @@ for k=1:body.numTriangles
         id2p(k,i); id2p(k,j); id2p(k,k); ];
     id2 = sort(unique(id2));
 end
+
+id2 = sort(unique(id2));
+c2j = rem(id2,np1);
+c2i = (id2-c2j)/np1;
+
+id3 = [];
+for k=1:body.numTriangles
+    tri = body.Triangles(k);
+    vs = tri.Vertices;
+    i = vs(1); j = vs(2); k = vs(3);
+    id3 = [ id3 ; ...
+        id3p(i,i,i); id3p(i,i,j); id3p(i,i,k);
+        id3p(i,j,i); id3p(i,j,j); id3p(i,j,k);
+        id3p(i,k,i); id3p(i,k,j); id3p(i,k,k);
+        id3p(j,i,i); id3p(j,i,j); id3p(j,i,k);
+        id3p(j,j,i); id3p(j,j,j); id3p(j,j,k);
+        id3p(j,k,i); id3p(j,k,j); id3p(j,k,k);
+        id3p(k,i,i); id3p(k,i,j); id3p(k,i,k);
+        id3p(k,j,i); id3p(k,j,j); id3p(k,j,k);
+        id3p(k,k,i); id3p(k,k,j); id3p(k,k,k) ];
+    id3 = sort(unique(id3));
+end
