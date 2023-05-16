ef2a6ca88f31371bbc58123e5e3b8b2d528b78a5
Mulugeta-Solomon <msolomon759@gmail.com>
2023-05-17 08:40:09 +0900

    update

--- 8th Report/SamplePrograms/two_dim_fea/calcuclate_coefficient_matrices_for_Green_strain_forces.m
+++ 8th Report/SamplePrograms/two_dim_fea/calcuclate_coefficient_matrices_for_Green_strain_forces.m
@@ -73,3 +73,32 @@ odot = @(x,y) [ x(1)*y; x(2)*y; x(3)*y ];
 odot3 = @(x,y,z) [ x(1)*y(1)*z; x(1)*y(2)*z; x(1)*y(3)*z; ...
                    x(2)*y(1)*z; x(2)*y(2)*z; x(2)*y(3)*z; ...
                    x(3)*y(1)*z; x(3)*y(2)*z; x(3)*y(3)*z ];
+
+coef_order_two = zeros(2*np, 3*np2);
+for k=1:body.numTriangles
+    tri = body.Triangles(k);
+    vs = tri.Vertices;
+    i = vs(1); j = vs(2); k = vs(3);
+    a = tri.vector_a; b = tri.vector_b;
+    cpl211 = (3/2)*a*odot(a,a)' + (1/2)*a*odot(b,b)' + b*odot(a,b)';
+    cpl212 = a*odot(a,b)' + b*odot(b,b)';
+    cpl213 = (1/2)*a*odot(a,a)' + (1/2)*a*odot(b,b)';
+    cpl221 = (1/2)*b*odot(b,b)' + (1/2)*b*odot(a,a)';
+    cpl222 = b*odot(a,b)' + a*odot(a,a)';
+    cpl223 = (3/2)*b*odot(b,b)' + (1/2)*b*odot(a,a)' + a*odot(b,a)';
+    cpm211 = 3*a*odot(a,a)' + a*odot(b,b)' + 2*b*odot(a,b)';
+    cpm212 = b*odot(a,a)' + a*odot(b,a)' + 2*b*odot(b,b)';
+    cpm213 = a*odot(a,a)' + b*odot(a,b)';
+    cpm221 = b*odot(b,b)' + a*odot(b,a)';
+    cpm222 = a*odot(b,b)' + b*odot(b,a)' + 2*a*odot(a,a)';
+    cpm223 = 3*b*odot(b,b)' + b*odot(a,a)' + 2*a*odot(b,a)';
+    mat = tri.lambda * tri.Area * tri.Thickness * [ cpl211, cpl212, cpl213; cpl221, cpl222, cpl223 ] + ...
+          tri.mu     * tri.Area * tri.Thickness * [ cpm211, cpm212, cpm213; cpm221, cpm222, cpm223 ];
+    ijk = [ i, j, k ];
+    row = [ ijk, np+ijk ];
+    ijk2 = [ find(id2==id2p(i,i)), find(id2==id2p(i,j)), find(id2==id2p(i,k)), ...
+             find(id2==id2p(j,i)), find(id2==id2p(j,j)), find(id2==id2p(j,k)), ...
+             find(id2==id2p(k,i)), find(id2==id2p(k,j)), find(id2==id2p(k,k)) ];
+    col = [ ijk2, np2+ijk2, 2*np2+ijk2 ];
+    coef_order_two(row,col) = coef_order_two(row,col) + mat;
+end
